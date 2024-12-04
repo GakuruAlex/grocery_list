7d598a4907e3645a29163f537f7b4c0c66381124
Alex Gakuru <gakurualexgicimo@gmail.com>
2024-12-04 12:51:38 +0300

    main

--- grocery.py
+++ grocery.py
@@ -8,7 +8,17 @@ class Grocery:
             return self.grocery_list
         else:
             self.grocery_list[item] = 1
-    
+   
+
+    def main(self):
+        while True:
+            try:
+                grocery_item: str = input("")
+            except EOFError:
+                print(self.print_grocery_items())
+                break
+            else:
+                self.add_grocery_item(grocery_item)
   
 if __name__ == "__main__":
     grocery = Grocery()
